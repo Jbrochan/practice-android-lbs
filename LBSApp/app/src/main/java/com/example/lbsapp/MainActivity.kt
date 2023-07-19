52e062c73184f57cf2cc9e131466ade633e9f02b
Jbrochan <hyeongchanj@gmail.com>
2023-07-19 15:17:14 +0900

    feat: Splash Screen 구현

--- LBSApp/app/src/main/java/com/example/lbsapp/MainActivity.kt
+++ LBSApp/app/src/main/java/com/example/lbsapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.lbsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.lbsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        installSplashScreen()
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
